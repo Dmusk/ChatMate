99b1cfa9f58b97f58ff94be8b2469b5479a6d6c6
Dmusk <aadarsh.wadile@mitaoe.ac.in>
2024-08-06 19:36:05 +0530

    setting up files

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello There Aadarsh");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Running on the port ${PORT}`);
+});
