5d35247c14e5c975f6f0226341cfd165a6bbc071
Dmusk <aadarsh.wadile@mitaoe.ac.in>
2024-08-07 15:46:54 +0530

    completed with the backend and frontend

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,31 +1,27 @@
-// import { useAuthContext } from "../../context/AuthContext";
-// import { extractTime } from "../../utils/extractTime";
-// import useConversation from "../../zustand/useConversation";
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
-// const Message = ({ message }) => {
-// 	const { authUser } = useAuthContext();
-// 	const { selectedConversation } = useConversation();
-// 	const fromMe = message.senderId === authUser._id;
-// 	const formattedTime = extractTime(message.createdAt);
-// 	const chatClassName = fromMe ? "chat-end" : "chat-start";
-// 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-// 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-// 	const shakeClass = message.shouldShake ? "shake" : "";
-
-const Message = () => {
+	const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
-		<div className='chat chat-end'>
+		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
 				<div className='w-10 rounded-full'>
-					<img
-						alt="Tailwind CSS chat bubble component"
-						src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
 			</div>
-			<div className={`text-white bg-blue-500 chat-bubble`}>Hi Whats Up</div>
-			<div className='flex items-center gap-1 text-xs opacity-50 chat-footer'>12:00</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className='flex items-center gap-1 text-xs opacity-50 chat-footer'>{formattedTime}</div>
 		</div>
 	);
 };
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,26 +1,60 @@
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
+import { useState } from "react";
+import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const { loading, signup } = useSignup();
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(inputs);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center mx-auto min-w-96'>
       <div className='w-full p-6 bg-gray-400 bg-opacity-0 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg'>
         <h1 className='text-3xl font-semibold text-center text-gray-300'>
-          Sign Up <span className='text-blue-500'> Chat-Mate</span>
+          Sign Up <span className='text-blue-500'> ChatApp</span>
         </h1>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className='p-2 label'>
               <span className='text-base label-text'>Full Name</span>
             </label>
-            <input type='text' placeholder='Aadarsh Wadile' className='w-full h-10 input input-bordered' />
+            <input
+              type='text'
+              placeholder='John Doe'
+              className='w-full h-10 input input-bordered'
+              value={inputs.fullName}
+              onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+            />
           </div>
 
           <div>
             <label className='p-2 label '>
               <span className='text-base label-text'>Username</span>
             </label>
-            <input type='text' placeholder='Aadarsh' className='w-full h-10 input input-bordered' />
+            <input
+              type='text'
+              placeholder='johndoe'
+              className='w-full h-10 input input-bordered'
+              value={inputs.username}
+              onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+            />
           </div>
 
           <div>
@@ -31,6 +65,8 @@ const SignUp = () => {
               type='password'
               placeholder='Enter Password'
               className='w-full h-10 input input-bordered'
+              value={inputs.password}
+              onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
             />
           </div>
 
@@ -42,17 +78,25 @@ const SignUp = () => {
               type='password'
               placeholder='Confirm Password'
               className='w-full h-10 input input-bordered'
+              value={inputs.confirmPassword}
+              onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
             />
           </div>
 
-          <GenderCheckbox />
+          <GenderCheckbox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender} />
 
-          <a className='inline-block mt-2 text-sm hover:underline hover:text-blue-600' href='#'>
+          <Link
+            to={"/login"}
+            className='inline-block mt-2 text-sm hover:underline hover:text-blue-600'
+            href='#'
+          >
             Already have an account?
-          </a>
+          </Link>
 
           <div>
-            <button className='mt-2 border btn btn-block btn-sm border-slate-700'>Sign Up</button>
+            <button className='mt-2 border btn btn-block btn-sm border-slate-700' disabled={loading}>
+              {loading ? <span className='loading loading-spinner'></span> : "Sign Up"}
+            </button>
           </div>
         </form>
       </div>
